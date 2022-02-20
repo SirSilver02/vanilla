58ce2c0760784fdaeacbcbecd38ef37309066f4d
Sir_Silver <mjcastro50@gmail.com>
2022-02-20 09:55:08 -0700

    Update vanilla.lua

--- vanilla.lua
+++ vanilla.lua
@@ -10,28 +10,21 @@ end
 local vanilla = {}
 vanilla.__index = vanilla
 
-function vanilla:init(aspect_width, aspect_height)
+function vanilla:init(aspect_width, aspect_height, settings)
     self.aspect_width = aspect_width
     self.aspect_height = aspect_height
-
-    self.canvas = nil
+    
+    self.scale = nil
+    self.canvas = love.graphics.newCanvas(aspect_width, aspect_height, settings)
     self.last_canvas = nil
-    self.canvas_settings = nil
 
     self:resize_canvas()
 end
 
-function vanilla:set_canvas_settings(settings)
-    self.canvas_settings = settings
-end
-
---note, this does remove the old canvas, so you will lose that data
 function vanilla:resize_canvas()
-    local scale = get_screen_scale(self.aspect_width, self.aspect_height)
-    self.canvas = love.graphics.newCanvas(self.aspect_width * scale, self.aspect_height * scale, self.canvas_settings)
+    self.scale = get_screen_scale(self.aspect_width, self.aspect_height)
 end
 
---call before drawing things you want rendered on the canvas
 function vanilla:start_draw()
     self.last_canvas = love.graphics.getCanvas()
 
@@ -39,22 +32,22 @@ function vanilla:start_draw()
     love.graphics.clear()
 end
 
---call when you're finished rendering to the canvas
 function vanilla:stop_draw()
     love.graphics.setCanvas(self.last_canvas)
 
     local w, h = love.graphics.getDimensions()
-    love.graphics.draw(self.canvas, w / 2, h / 2, 0, 1, 1, self.canvas:getWidth() / 2, self.canvas:getHeight() / 2)
+    love.graphics.draw(self.canvas, w / 2, h / 2, 0, self.scale, self.scale, self.canvas:getWidth() / 2, self.canvas:getHeight() / 2)
+    --love.graphics.draw(self.canvas, 0, 0, 0, self.scale, self.scale)
 end
 
---converts mouse coords to canvas coords, assumes canvas is being rendered in the center of the screen, thats how this vanilla is intended to work.
-function vanilla:mouse_to_canvas()
-    local mx, my = love.mouse.getPosition()
+function vanilla:mouse_to_canvas(x, y)
+    local mx, my = x or love.mouse.getX(), y or love.mouse.getY()
 
     local scr_w, scr_h = love.graphics.getDimensions()
     local canvas_w, canvas_h = self.canvas:getDimensions()
+    canvas_w, canvas_h = canvas_w * self.scale, canvas_h * self.scale
 
-    return mx - (scr_w / 2) + (canvas_w / 2), my - (scr_h / 2) + (canvas_h / 2)
+    return (mx / self.scale) - (scr_w / 2) / self.scale + (canvas_w / 2) / self.scale , (my / self.scale) - (scr_h / 2) / self.scale + (canvas_h / 2) / self.scale
 end
 
 --love callback
@@ -62,9 +55,9 @@ function vanilla:resize(w, h)
     self:resize_canvas()
 end
 
-return {new = function(aspect_width, aspect_height)
+return {new = function(aspect_width, aspect_height, settings)
     local inst = setmetatable({}, vanilla)
-    inst:init(aspect_width, aspect_height)
+    inst:init(aspect_width, aspect_height, settings)
 
     return inst
 end}
