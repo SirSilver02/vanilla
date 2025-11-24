b21c48a6853ae4dd86072e650daf3e9b94119526
Sir_Silver <mjcastro50@gmail.com>
2025-11-24 04:51:53 -0700

    Code cleanup.

--- vanilla.lua
+++ vanilla.lua
@@ -7,36 +7,30 @@ end
 local vanilla = {}
 vanilla.__index = vanilla
 
-function vanilla:init(aspect_width, aspect_height, settings)
-    self.aspect_width = aspect_width
-    self.aspect_height = aspect_height
+function vanilla:init(width, height, settings)
+    self.width = width
+    self.height = height
     
     self.scale = nil
     self.last_canvas = nil
-    self.canvas = love.graphics.newCanvas(aspect_width, aspect_height, settings)
+    self.canvas = love.graphics.newCanvas(width, height, settings)
 
     self:resize_canvas()
 end
 
-function vanilla:resize_canvas()
-    self.scale = get_screen_scale(self.aspect_width, self.aspect_height)
-end
-
-function vanilla:start_draw()
+function vanilla:draw(callback)
     self.last_canvas = love.graphics.getCanvas()
 
     love.graphics.setCanvas(self.canvas)
-    love.graphics.clear()
-end
-
-function vanilla:stop_draw()
+        love.graphics.clear()
+        callback()
     love.graphics.setCanvas(self.last_canvas)
 
     local w, h = love.graphics.getDimensions()
     love.graphics.draw(self.canvas, w / 2, h / 2, 0, self.scale, self.scale, self.canvas:getWidth() / 2, self.canvas:getHeight() / 2)
-    --love.graphics.draw(self.canvas, 0, 0, 0, self.scale, self.scale)
 end
 
+--global mouse position to canvas position
 function vanilla:mouse_to_canvas(x, y)
     local mx, my = x or love.mouse.getX(), y or love.mouse.getY()
 
@@ -47,14 +41,17 @@ function vanilla:mouse_to_canvas(x, y)
     return (mx / self.scale) - (scr_w / 2) / self.scale + (canvas_w / 2) / self.scale , (my / self.scale) - (scr_h / 2) / self.scale + (canvas_h / 2) / self.scale
 end
 
---love callback
+--call in love.resize(w, h)
 function vanilla:resize(w, h)
-    self:resize_canvas()
+    self.scale = get_screen_scale(self.width, self.height, w, h)
 end
 
-return {new = function(aspect_width, aspect_height, settings)
-    local inst = setmetatable({}, vanilla)
-    inst:init(aspect_width, aspect_height, settings)
+return {
+    new = function(width, height, settings)
+        local inst = setmetatable({}, vanilla)
+        inst:init(width, height, settings)
 
-    return inst
-end}
+        return inst
+    end
+}
+
